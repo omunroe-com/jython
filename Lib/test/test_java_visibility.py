8e2db5077929a9c31e54cb3050a6330f564f4eeb
Charlie Groves <charlie.groves@gmail.com>
2008-12-13 07:33:55 +0000

    Move test087 into test_java_integration, and test088 and test090 are tested by test_java_integration and test_imp_jy respectively

---
+++ Lib/test/test_java_visibility.py
@@ -0,0 +1,89 @@
+import unittest
+from test import test_support
+from org.python.tests import InterfaceCombination, Invisible, SubVisible, Visible, VisibleOverride
+from org.python.tests import VisibilityResults as Results
+
+class VisibilityTest(unittest.TestCase):
+    def test_invisible(self):
+        for item in dir(Invisible):
+            self.assert_(not item.startswith("package"))
+            self.assert_(not item.startswith("private"))
+            self.assert_(not item.startswith("protected"))
+
+    def test_protected_from_python_subclass(self):
+        class SubVisible(Visible):
+            def __init__(self, publicValue=None):
+                if publicValue is not None:
+                    Visible.__init__(self, publicValue)
+                else:
+                    Visible.__init__(self)
+        # TODO - protectedStaticMethod, protectedStaticField, StaticInner, and protectedField should
+        # be here
+        s = SubVisible()
+        self.assertEquals(Results.PROTECTED_METHOD, s.protectedMethod(0))
+        self.assertEquals(Results.OVERLOADED_PROTECTED_METHOD, s.protectedMethod('foo'))
+        self.assertEquals(Results.UNUSED, SubVisible(Results.UNUSED).visibleField)
+
+    def test_visible(self):
+        v = Visible()
+        self.assertEquals(Results.PUBLIC_FIELD, v.visibleField)
+        self.assertEquals(Results.PUBLIC_STATIC_FIELD, Visible.visibleStaticField)
+        self.assertEquals(Results.PUBLIC_METHOD, v.visibleInstance(0))
+        self.assertEquals(Results.OVERLOADED_PUBLIC_METHOD, v.visibleInstance('a'))
+        self.assertEquals(Results.EXTRA_ARG_PUBLIC_METHOD, v.visibleInstance(0, 'b'))
+        self.assertEquals(Results.OVERLOADED_EXTRA_ARG_PUBLIC_METHOD,
+                v.visibleInstance('a', 'b'))
+        self.assertEquals(Results.PUBLIC_STATIC_METHOD, Visible.visibleStatic(0))
+        self.assertEquals(Results.OVERLOADED_PUBLIC_STATIC_METHOD, 
+                v.visibleStatic('a'))
+        self.assertEquals(Results.EXTRA_ARG_PUBLIC_STATIC_METHOD,
+                v.visibleStatic(0, 'a'))
+        self.assertEquals(Results.PUBLIC_STATIC_FIELD, Visible.StaticInner.visibleStaticField)
+
+        # Ensure that the visibleInstance method from SubVisible that takes a double doesn't
+        # leak through to the parent
+        self.assertRaises(TypeError, v.visibleInstance, 0.0, 'b')
+        # TODO - no way to access a field with the same name as a method
+        #self.assertEquals(Results.PUBLIC_METHOD_FIELD, v.visibleInstance)
+        #self.assertEquals(Results.PUBLIC_STATIC_METHOD_FIELD, Visible.visibleStatic)
+
+    def test_java_subclass(self):
+        s = SubVisible()
+        self.assertEquals(Results.PUBLIC_FIELD, s.visibleField)
+        self.assertEquals(Results.PUBLIC_STATIC_FIELD, SubVisible.visibleStaticField)
+        self.assertEquals(Results.SUBCLASS_STATIC_OVERRIDE, SubVisible.visibleStatic(3))
+        self.assertEquals(Results.SUBCLASS_STATIC_OVERLOAD, SubVisible.visibleStatic(3.0, 'a'))
+        self.assertEquals(Results.SUBCLASS_OVERRIDE, s.visibleInstance(3))
+        self.assertEquals(Results.SUBCLASS_OVERLOAD, s.visibleInstance(3.0, 'a'))
+        self.assertEquals(Results.PACKAGE_METHOD, s.packageMethod())
+        # Java methods don't allow direct calling of the superclass method, so it should 
+        # return the subclass value here.
+        self.assertEquals(Results.SUBCLASS_OVERRIDE, Visible.visibleInstance(s, 3))
+        self.assertEquals(Results.PUBLIC_STATIC_FIELD, SubVisible.StaticInner.visibleStaticField)
+
+
+    def test_in_dict(self):
+        for c in Visible, SubVisible, VisibleOverride:
+            self.failUnless('visibleInstance' in c.__dict__,
+                    'visibleInstance expected in %s __dict__' % c)
+
+    def test_interface_combination(self):
+        '''Checks that a private class that extends a public class and public interfaces has only the items
+           from the public bases visible'''
+        i = InterfaceCombination.newImplementation()
+        self.assertEquals(InterfaceCombination.NO_ARG_RESULT, i.getValue(),
+                "methods from IFace should be visible on Implementation")
+        self.assertEquals(InterfaceCombination.ONE_ARG_RESULT, i.getValue("one arg"),
+                "methods from IIFace should be visible on Implementation")
+        self.assertEquals(InterfaceCombination.TWO_ARG_RESULT, i.getValue("one arg", "two arg"),
+                "methods from Base should be visible on Implementation")
+        self.assertRaises(TypeError, i.getValue, "one arg", "two arg", "three arg", 
+                "methods defined solely on Implementation shouldn't be visible")
+        self.assertFalse(hasattr(i, "internalMethod"),
+                "methods from private interfaces shouldn't be visible on a private class")
+
+def test_main():
+    test_support.run_unittest(VisibilityTest)
+
+if __name__ == "__main__":
+    test_main()
---
+++ src/org/python/core/PyReflectedFunction.java
@@ -0,0 +1,286 @@
+// Copyright (c) Corporation for National Research Initiatives
+package org.python.core;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Set;
+
+import org.python.util.Generic;
+
+public class PyReflectedFunction extends PyObject {
+
+    public String __name__;
+
+    public PyObject __doc__ = Py.None;
+
+    public ReflectedArgs[] argslist = new ReflectedArgs[1];
+
+    public int nargs;
+
+    protected PyReflectedFunction(String name) {
+        __name__ = name;
+    }
+
+    public PyReflectedFunction(Method... methods) {
+        this(methods[0].getName());
+        for (Method meth : methods) {
+            addMethod(meth);
+        }
+        if (nargs == 0) {
+            String msg = String.format("Attempted to make Java method visible, but it isn't "
+                                               + "callable[method=%s, class=%s]",
+                                       methods[0].getName(),
+                                       methods[0].getDeclaringClass());
+            throw Py.SystemError(msg);
+        }
+    }
+
+    public PyObject _doget(PyObject container) {
+        return _doget(container, null);
+    }
+
+    public PyObject _doget(PyObject container, PyObject wherefound) {
+        if (container == null) {
+            return this;
+        }
+        return new PyMethod(this, container, wherefound);
+    }
+
+    public boolean _doset(PyObject container) {
+        throw Py.TypeError("java function not settable: " + __name__);
+    }
+
+    public PyObject getDoc() {
+        return __doc__;
+    }
+
+    private ReflectedArgs makeArgs(Method m) {
+        return new ReflectedArgs(m,
+                                 m.getParameterTypes(),
+                                 m.getDeclaringClass(),
+                                 Modifier.isStatic(m.getModifiers()));
+    }
+
+    public PyReflectedFunction copy() {
+        PyReflectedFunction func = new PyReflectedFunction(__name__);
+        func.__doc__ = __doc__;
+        func.nargs = nargs;
+        func.argslist = new ReflectedArgs[nargs];
+        System.arraycopy(argslist, 0, func.argslist, 0, nargs);
+        return func;
+    }
+
+    public boolean handles(Method method) {
+        return handles(makeArgs(method));
+    }
+
+    protected boolean handles(ReflectedArgs args) {
+        for (int i = 0; i < nargs; i++) {
+            int cmp = args.compareTo(argslist[i]);
+            if (cmp == 0) {
+                return true;
+            } else if (cmp == 1) {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    public void addMethod(Method m) {
+        // Only add public methods unless we're overriding
+        if (!Modifier.isPublic(m.getModifiers()) && Options.respectJavaAccessibility) {
+            return;
+        }
+        addArgs(makeArgs(m));
+    }
+
+    protected void addArgs(ReflectedArgs args) {
+        int i;
+        for (i = 0; i < nargs; i++) {
+            int cmp = args.compareTo(argslist[i]);
+            if (cmp == 0) {
+                return;
+            } else if (cmp == ReflectedArgs.REPLACE) {
+                argslist[i] = args;
+                return;
+            } else if (cmp == -1) {
+                break;
+            }
+        }
+        int nn = nargs + 1;
+        if (nn > argslist.length) {
+            ReflectedArgs[] newargslist = new ReflectedArgs[nn + 2];
+            System.arraycopy(argslist, 0, newargslist, 0, nargs);
+            argslist = newargslist;
+        }
+        for (int j = nargs; j > i; j--) {
+            argslist[j] = argslist[j - 1];
+        }
+        argslist[i] = args;
+        nargs = nn;
+    }
+
+    public PyObject __call__(PyObject self, PyObject[] args, String[] keywords) {
+        ReflectedCallData callData = new ReflectedCallData();
+        Object method = null;
+        ReflectedArgs[] argsl = argslist;
+        int n = nargs;
+        for (int i = 0; i < n; i++) {
+            ReflectedArgs rargs = argsl[i];
+            // System.err.println(rargs.toString());
+            if (rargs.matches(self, args, keywords, callData)) {
+                method = rargs.data;
+                break;
+            }
+        }
+        if (method == null) {
+            throwError(callData.errArg, args.length, self != null, keywords.length != 0);
+        }
+        Object cself = callData.self;
+        Method m = (Method)method;
+        Object o;
+        try {
+            o = m.invoke(cself, callData.getArgsArray());
+        } catch (Throwable t) {
+            throw Py.JavaError(t);
+        }
+        return Py.java2py(o);
+    }
+
+    public PyObject __call__(PyObject[] args, String[] keywords) {
+        return __call__(null, args, keywords);
+    }
+
+    // A bunch of code to make error handling prettier
+    protected void throwError(String message) {
+        throw Py.TypeError(__name__ + "(): " + message);
+    }
+
+    private static void addRange(StringBuilder buf, int min, int max, String sep) {
+        if (buf.length() > 0) {
+            buf.append(sep);
+        }
+        if (min < max) {
+            buf.append(Integer.toString(min)).append('-').append(max);
+        } else {
+            buf.append(min);
+        }
+    }
+
+    protected void throwArgCountError(int nArgs, boolean self) {
+        // Assume no argument lengths greater than 40...
+        boolean[] legalArgs = new boolean[40];
+        int maxArgs = -1;
+        int minArgs = 40;
+        for (int i = 0; i < nargs; i++) {
+            ReflectedArgs rargs = argslist[i];
+            int l = rargs.args.length;
+            if (!self && !rargs.isStatic) {
+                l += 1;
+            }
+            legalArgs[l] = true;
+            if (l > maxArgs) {
+                maxArgs = l;
+            }
+            if (l < minArgs) {
+                minArgs = l;
+            }
+        }
+        StringBuilder buf = new StringBuilder();
+        int startRange = minArgs;
+        int a = minArgs + 1;
+        while (a < maxArgs) {
+            if (legalArgs[a]) {
+                a++;
+                continue;
+            } else {
+                addRange(buf, startRange, a - 1, ", ");
+                a++;
+                while (a <= maxArgs) {
+                    if (legalArgs[a]) {
+                        startRange = a;
+                        break;
+                    }
+                    a++;
+                }
+            }
+        }
+        addRange(buf, startRange, maxArgs, " or ");
+        throwError("expected " + buf + " args; got " + nArgs);
+    }
+
+    private static String ordinal(int n) {
+        switch(n + 1){
+            case 0:
+                return "self";
+            case 1:
+                return "1st";
+            case 2:
+                return "2nd";
+            case 3:
+                return "3rd";
+            default:
+                return Integer.toString(n + 1) + "th";
+        }
+    }
+
+    private static String niceName(Class arg) {
+        if (arg == String.class || arg == PyString.class) {
+            return "String";
+        }
+        if (arg.isArray()) {
+            return niceName(arg.getComponentType()) + "[]";
+        }
+        return arg.getName();
+    }
+
+    protected void throwBadArgError(int errArg, int nArgs, boolean self) {
+        Set<Class<?>> argTypes = Generic.set();
+        for (int i = 0; i < nargs; i++) {
+            // if (!args.isStatic && !self) { len = len-1; }
+            // This check works almost all the time.
+            // I'm still a little worried about non-static methods
+            // called with an explict self...
+            if (argslist[i].args.length == nArgs) {
+                if (errArg == ReflectedCallData.UNABLE_TO_CONVERT_SELF) {
+                    argTypes.add(argslist[i].declaringClass);
+                } else {
+                    argTypes.add(argslist[i].args[errArg]);
+                }
+            }
+        }
+        StringBuilder buf = new StringBuilder();
+        for (Class<?> arg : argTypes) {
+            buf.append(niceName(arg));
+            buf.append(", ");
+        }
+        if (buf.length() > 2) {
+            buf.setLength(buf.length() - 2);
+        }
+        throwError(ordinal(errArg) + " arg can't be coerced to " + buf);
+    }
+
+    protected void throwError(int errArg, int nArgs, boolean self, boolean keywords) {
+        if (keywords) {
+            throwError("takes no keyword arguments");
+        } else if (errArg == ReflectedCallData.BAD_ARG_COUNT) {
+            throwArgCountError(nArgs, self);
+        } else {
+            throwBadArgError(errArg, nArgs, self);
+        }
+    }
+
+    // Included only for debugging purposes...
+    public void printArgs() {
+        System.err.println("nargs: " + nargs);
+        for (int i = 0; i < nargs; i++) {
+            ReflectedArgs args = argslist[i];
+            System.err.println(args.toString());
+        }
+    }
+
+    public String toString() {
+        // printArgs();
+        return "<java function " + __name__ + " " + Py.idstr(this) + ">";
+    }
+}
