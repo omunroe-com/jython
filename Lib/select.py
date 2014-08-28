62efefb4b6f47201f2b968b81eb3f007d983c8fd
Stefan Richthofer <stefan.richthofer@gmx.de>
2014-08-28 06:06:02 +0200

    Modified __ensure_finalizer__ to work without using reflection and added some paragraphs to the documentation in FinalizablePyObject (stating use cases fo __ensure_finalizer__ and specifying that it is not possible to overwrite __ensure_finalizer__ on Python side).

---
+++ Lib/select.py
@@ -0,0 +1,12 @@
+# dispatches to _socket for actual implementation
+
+from _socket import (
+    POLLIN,
+    POLLOUT,
+    POLLPRI,
+    POLLERR,
+    POLLHUP,
+    POLLNVAL,
+    error,
+    poll,
+    select)
